9b26a0334b10351166ab263b2093bf2f324174b6
LEE WHOO SEONG <124110982+JayMon0327@users.noreply.github.com>
2023-07-14 23:59:30 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int solution(String message) {
+        int result = message.length();
+        int answer = result*2;
+        return answer;
+    }
+}
