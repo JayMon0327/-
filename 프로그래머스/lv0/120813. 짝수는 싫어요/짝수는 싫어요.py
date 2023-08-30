e3eb627bc2968ca6f0c4ea1548282145b4be9e46
LEE WHOO SEONG <kioplm0211@naver.com>
2023-08-30 22:16:08 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,8 +1,3 @@
 def solution(n):
-    array=[]
-    for i in range(1, n+1):
-        if i % 2 == 1:
-            array.append(i)
-    answer = array
-    
-    return answer
+    return [i for i in range(1, n+1, 2)]
+
