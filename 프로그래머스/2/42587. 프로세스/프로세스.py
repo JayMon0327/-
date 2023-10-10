50548a02b7397fba635ced4eebfdd1de5431aac3
LEE WHOO SEONG <kioplm0211@naver.com>
2023-10-10 17:49:27 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,13 +2,13 @@ from collections import deque
 
 def solution(priorities, location):
     answer = 0
-    queue = deque([(v, i) for i, v in enumerate(priorities)])  # (중요도, 인덱스) 쌍으로 큐에 저장
+    queue = deque([(v, i) for i, v in enumerate(priorities)]) 
     while queue:
         front = queue.popleft()
-        if any(front[0] < q[0] for q in queue):  # 대기중인 프로세스 중 우선순위가 더 높은 경우
+        if any(front[0] < q[0] for q in queue): 
             queue.append(front)
         else:
             answer += 1
-            if front[1] == location:  # 찾고자 하는 프로세스인 경우
+            if front[1] == location: 
                 break
     return answer
